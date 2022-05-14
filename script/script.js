2969a170c44c7020fbdb12bacaf8f99786266b00
OlekBliter <ggudok054@gmail.com>
2022-05-14 22:42:04 +0300

    add dark mode

--- script/script.js
+++ script/script.js
@@ -1,3 +1,46 @@
+//change Mode
+
+let mode = 0
+
+function changeMode(){   
+    document.querySelector('.changeMode').style.animation = 'none'
+    if(mode==0){      
+        mode++
+        document.querySelector('.changeMode').style.animation = 'changeModeAnimationDark 1s forwards'
+        document.querySelector('.wrap').style.backgroundImage = 'linear-gradient(90deg, rgb(59, 18, 97), rgb(128, 68, 128))'
+        document.querySelector('#card1').style.background = 'rgb(85, 84, 84)'
+        document.querySelector('#card2').style.background = 'rgb(85, 84, 84)'
+        document.querySelector('#card3').style.background = 'rgb(85, 84, 84)'
+        document.querySelector('*').style.color = 'white'
+        document.querySelector('.shopTxt').style.color = 'white'
+        document.querySelector('.achTxt').style.color = 'white'
+        document.querySelector('.achievements').style.background = 'rgb(128, 68, 128)'
+        document.querySelector('.shop').style.background = 'rgb(128, 68, 128)'
+        document.querySelector('#flip').style.background = 'blueviolet'
+        document.querySelector('#flip').style.border = '1.5px solid blueviolet'
+        document.querySelector('#reset').style.background = 'rgb(85, 84, 84)'
+        document.querySelector('#reset').style.border = '1.5px solid blueviolet'
+        document.querySelector('#reset').style.color = 'blueviolet'
+    }else{
+        mode--
+        document.querySelector('.changeMode').style.animation = 'changeModeAnimationLight 1s forwards'
+        document.querySelector('.wrap').style.backgroundImage = 'linear-gradient(90deg, rgb(44, 211, 44), white)'
+        document.querySelector('#card1').style.background = 'white'
+        document.querySelector('#card2').style.background = 'white'
+        document.querySelector('#card3').style.background = 'white'
+        document.querySelector('*').style.color = 'black'
+        document.querySelector('.shopTxt').style.color = 'black'
+        document.querySelector('.achTxt').style.color = 'black'
+        document.querySelector('.achievements').style.background = 'rgb(40, 209, 40)'
+        document.querySelector('.shop').style.background = 'rgb(40, 209, 40)'
+        document.querySelector('#flip').style.background = 'green'
+        document.querySelector('#flip').style.border = '1.5px solid green'
+        document.querySelector('#reset').style.background = 'aliceblue'
+        document.querySelector('#reset').style.border = '1.5px solid green'
+        document.querySelector('#reset').style.color = 'green'
+    }
+}
+
 // main js
 
 let heads = 0;
